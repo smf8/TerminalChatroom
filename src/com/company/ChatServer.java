ec2dde544f650cc93039134b3f0287cd65464921
Mohamad <mr.smf8@gmail.com>
2019-05-27 13:58:05 +0430

    Added code for chat server to deal with connections

--- src/com/company/ChatServer.java
+++ src/com/company/ChatServer.java
@@ -1,8 +1,17 @@
 package com.company;
 
-public class Main {
-        private static final int PORT = 8585;
-    public static void main(String[] args) {
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.util.concurrent.Executors;
 
+public class ChatServer {
+        private static final int PORT = 8585;
+
+    public static void main(String[] args) throws IOException {
+        ServerSocket serverSocket = new ServerSocket(8585);
+        var threadPool = Executors.newFixedThreadPool(10);
+        while(true){
+            threadPool.execute(new ChatHandler(serverSocket.accept()));
+        }
     }
 }
