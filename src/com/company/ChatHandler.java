3b5aaaf0dcd2212f426b9cef04fd6fbccb056912
Mohamad <mr.smf8@gmail.com>
2019-05-27 13:56:54 +0430

    Created a user handler to handle connected users

--- src/com/company/ChatHandler.java
+++ src/com/company/ChatHandler.java
@@ -1,4 +1,41 @@
 package com.company;
 
-public class ChatHandler {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.ArrayList;
+
+public class ChatHandler implements Runnable {
+
+    private PrintWriter writer;
+    private BufferedReader reader;
+    private String username;
+    ArrayList<String> usernames;
+    private static ArrayList<PrintWriter> userWriters = new ArrayList<>();
+
+    @Override
+    public void run() {
+        while (true){
+            String input = null;
+            while(true){
+                try {
+                    if (!((input = reader.readLine()) != null)) break;
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                    for (PrintWriter printWriter : userWriters){
+                        printWriter.println(username  + " : " + input);
+                    }
+            }
+        }
+    }
+
+    public ChatHandler(Socket socket) throws IOException {
+        this.writer = new PrintWriter(socket.getOutputStream(), true);
+        this.reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        this.username = reader.readLine();
+        userWriters.add(writer);
+    }
 }
